07a221cc005af96955c199f669e0be72bca07a3f
Zuhair Sakharkar <sakharkarzuhair210@gmail.com>
2023-08-09 22:38:35 +0530

    Fixed Error Of Material UI

--- src/index.js
+++ src/index.js
@@ -1,18 +1,27 @@
 import React from "react";
 import ReactDom from "react-dom";
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware, compose } from "redux";
+import {
+  legacy_createStore as createStore,
+  applyMiddleware,
+  compose,
+} from "redux";
 import thunk from "redux-thunk";
+import { createTheme } from "@mui/material";
+import { ThemeProvider } from "@mui/styles";
 
 import reducers from "./reducers";
 
 import App from "./App";
 
 const store = createStore(reducers, compose(applyMiddleware(thunk)));
+const theme = createTheme();
 
 ReactDom.render(
   <Provider store={store}>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </Provider>,
   document.getElementById("root")
 );
